406bcb8e60cc3173cb6eea247c1332b8b71323a2
Shantanu Godbole <46984743+shantanugodbole@users.noreply.github.com>
2021-03-03 12:09:09 +0530

    endpoint added

--- app.py
+++ app.py
@@ -7,39 +7,46 @@ import pickle
 
 app = Flask(__name__)
 
+key = {0: 'Fungal infection', 1: 'Allergy', 2: 'GERD', 3: 'Chronic cholestasis', 4: 'Drug Reaction',
+       5: 'Peptic ulcer diseae', 6: 'AIDS', 7: 'Diabetes ', 8: 'Gastroenteritis', 9: 'Bronchial Asthma', 10: 'Hypertension ',
+       11: 'Migraine', 12: 'Cervical spondylosis',
+       13: 'Paralysis (brain hemorrhage)', 14: 'Jaundice', 15: 'Malaria', 16: 'Chicken pox', 17: 'Dengue', 18: 'Typhoid', 19: 'hepatitis A',
+       20: 'Hepatitis B', 21: 'Hepatitis C', 22: 'Hepatitis D', 23: 'Hepatitis E', 24: 'Alcoholic hepatitis', 25: 'Tuberculosis',
+       26: 'Common Cold', 27: 'Pneumonia', 28: 'Dimorphic hemmorhoids(piles)', 29: 'Heart attack', 30: 'Varicose veins', 31: 'Hypothyroidism',
+       32: 'Hyperthyroidism', 33: 'Hypoglycemia', 34: 'Osteoarthristis', 35: 'Arthritis',
+       36: '(vertigo) Paroymsal  Positional Vertigo', 37: 'Acne', 38: 'Urinary tract infection', 39: 'Psoriasis',
+       40: 'Impetigo'}
 
-# @app.route('/predict/<test>')
-# def default(test):
-#     return "Testing Flask App" + test
+
+@app.route('/test/<test>')
+def default(test):
+    return "Testing Flask App" + test
 
 
-@app.route('/predict', methods=['GET'])
-def main():
-    # symptoms = []
-    # symptoms.append(request.args.get('s1', None))
-    # symptoms.append(request.args.get('s2',))
-    # symptoms.append(request.args.get('s3'))
-    # symptoms.append(request.args.get('s4'))
-    # symptoms.append(request.args.get('s5'))
+@app.route('/predict/<s1>/<s2>/<s3>/<s4>/<s5>')
+def predict(s1, s2, s3, s4, s5):
+    symptoms = []
+    symptoms.append(s1)
+    symptoms.append(s2)
+    symptoms.append(s3)
+    symptoms.append(s4)
+    symptoms.append(s5)
+    # return s1
+    l2 = []
+    for x in range(0, len(l1)):
+        l2.append(0)
 
-    print(request.args.get('s1'))
+    for k in range(0, len(l1)):
+        for z in symptoms:
+            if(z == l1[k]):
+                l2[k] = 1
 
-    # print(symptoms)
-
-    # l2 = []
-    # for x in range(0, len(l1)):
-    #     l2.append(0)
-
-    # for k in range(0, len(l1)):
-    #     for z in symptoms:
-    #         if(z == l1[k]):
-    #             l2[k] = 1
-
-    # infile = open('finalized_model.pickle', 'rb')
-    # model = pickle.load(infile)
-    # infile.close()
-
-    # return jsonify(model.predict([l2]))
+    infile = open('finalized_model.pickle', 'rb')
+    model = pickle.load(infile)
+    predicted = tuple(model.predict([l2]).tolist())
+    # print(predicted)
+    return json.dumps(key[predicted[0]])
+    infile.close()
 
 
 if __name__ == '__main__':
